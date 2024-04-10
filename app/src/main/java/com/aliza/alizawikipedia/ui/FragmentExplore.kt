d9f1cc8589a7477407eeee59df8f212df251b0e8
Aliza <alizashahsavari@gmail.com>
2024-04-10 22:08:06 +0330

    Creating three main pages of the program in the form of three Fragments

---
+++ app/src/main/java/com/aliza/alizawikipedia/ui/FragmentExplore.kt
@@ -0,0 +1,15 @@
+package com.aliza.alizawikipedia.ui
+
+import android.os.Bundle
+import android.view.View
+import com.aliza.alizawikipedia.base.BaseFragment
+import com.aliza.alizawikipedia.databinding.FragmentExploreBinding
+
+class FragmentExplore : BaseFragment<FragmentExploreBinding>(
+    FragmentExploreBinding::inflate
+) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+}
---
+++ app/src/main/java/com/aliza/alizawikipedia/ui/FragmentTrend.kt
@@ -0,0 +1,16 @@
+package com.aliza.alizawikipedia.ui
+
+import android.os.Bundle
+import android.view.View
+import com.aliza.alizawikipedia.base.BaseFragment
+import com.aliza.alizawikipedia.databinding.FragmentTrendBinding
+
+class FragmentTrend : BaseFragment<FragmentTrendBinding>(
+    FragmentTrendBinding::inflate
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+}
