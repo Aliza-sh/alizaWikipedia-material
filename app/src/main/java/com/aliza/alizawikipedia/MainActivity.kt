3e530655dc0ad553f7cccefd2b10993f72cf0515
Aliza <alizashahsavari@gmail.com>
2024-04-10 22:10:03 +0330

    Implementation of BottomNavigation logic in MainActivity

--- app/src/main/java/com/aliza/alizawikipedia/MainActivity.kt
+++ app/src/main/java/com/aliza/alizawikipedia/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.aliza.alizawikipedia.base.BaseActivity
 import com.aliza.alizawikipedia.databinding.ActivityMainBinding
+import com.aliza.alizawikipedia.ui.FragmentExplore
+import com.aliza.alizawikipedia.ui.FragmentProfile
+import com.aliza.alizawikipedia.ui.FragmentTrend
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun inflateBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -13,9 +17,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(binding.toolBarMain)
+        firstRun()
 
         setIconNavigationDrawer()
         setNavigationDrawerMenuOnClickListener()
+        setNavigationBottomOnClickListener()
+        binding.bottomNavigationMain.setOnItemReselectedListener {}
 
     }
 
@@ -62,5 +69,31 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             }
             true
         }
+    }
+
+    private fun setNavigationBottomOnClickListener() {
+        binding.bottomNavigationMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_explore -> {
+                    replaceFragment(FragmentExplore())
+                }
+                R.id.menu_trend -> {
+                    replaceFragment(FragmentTrend())
+                }
+                R.id.menu_profile -> {
+                    replaceFragment(FragmentProfile())
+                }
+            }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameMainContainer, fragment)
+            .commit()
+    }
+    private fun firstRun() {
+        replaceFragment(FragmentExplore())
+        binding.bottomNavigationMain.selectedItemId = R.id.menu_explore
     }
 }
