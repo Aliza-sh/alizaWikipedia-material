3f8f6de14f86dd8321bf0bc93fb11cf367ef1608
Aliza <alizashahsavari@gmail.com>
2024-04-16 00:05:02 +0330

    Add parcelize plugin to convert data in their transfer 1. Its implementation is in the data class ItemPost and returns up to a Parcelable object.

--- app/src/main/java/com/aliza/alizawikipedia/data/ItemPost.kt
+++ app/src/main/java/com/aliza/alizawikipedia/data/ItemPost.kt
@@ -1,5 +1,9 @@
 package com.aliza.alizawikipedia.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ItemPost(
     val imgUrl: String,
     val txtTitle: String,
@@ -9,4 +13,4 @@ data class ItemPost(
     // for trend fragment =>
     val isTrend :Boolean ,
     val insight :String
-)
+): Parcelable
